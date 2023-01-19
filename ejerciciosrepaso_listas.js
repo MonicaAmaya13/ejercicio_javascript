29f36623f1c535c4bab387379cc7decaf24083f2
MonicaAmaya13 <115158073+MonicaAmaya13@users.noreply.github.com>
2023-01-19 14:33:29 +0100

    Add files via upload

---
+++ ejerciciosrepaso_listas.js
@@ -0,0 +1,33 @@
+//Crea una lista de números del 1 
+// al 10 utlizando un bucle for 
+numeros_lista=[]; 
+for(var i=1; i<10; i=i+1){
+   numeros_lista.push(i); 
+}
+
+//lista de frutas y verduras con array literal 
+var listaverduras = ["TOMATE", "BROCOLI", "LECHUGA"];
+//añade un elemento al final de la lista anterior 
+listaverduras.push("cebolla");
+//cambia el valor del tercer elemento 
+listaverduras.splice(2, 1, "berenjena"); 
+//recorre la lista con bucle for 
+for(var x = 0; x<10; x=x+1){
+    console.log(lista[x])
+}
+// crea una función que reciba una lista como parámetro y devuelva el número de elementos de la lista
+fuction ultimo_elemento(lista) {
+    console.log(lista[lista,length-1]); 
+}
+//Crea una función que reciba una lista y un elemento como parámetros y devuelva verdadero si es el elemento
+// se encuentra en la lista, o faslso en caso contario
+fuction seEncuentraElementoENlista(lista, elemento){
+
+}
+    for(var i=0; i<lista.length; i++){
+
+    }
+        if(lista[i] == elemento){
+            return true; 
+        }
+            
